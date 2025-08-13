349d3c364a94261b5e33425c301905b273cefb0e
Ubuntu <ubuntu@ip-172-31-30-49.us-east-2.compute.internal>
2025-08-13 14:08:19 +0000

    Added my script

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this is a greeting conersation script
+
+
+echo "Person A: Hi, nice to meet you"
+echo "Person B: Hello! Nice to meet you too"
+echo "Person A: How are you?"
+echo "Person B: I’m good, thank you. How about you?"
+echo "Person A: I’m doing well, thanks"
+echo "Person B: That’s great to hear"
