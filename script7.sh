f8a64520ca41b972772b83285b8f541ac7fcdc01
Ubuntu <ubuntu@ip-172-31-30-238.us-east-2.compute.internal>
2025-08-15 14:11:12 +0000

    added my script

---
+++ script7.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "Enter a fruit name: " fruit
+
+case $fruit in
+    apple)
+        echo "Apples are red or green." ;;
+    banana)
+        echo "Bananas are yellow." ;;
+    orange)
+        echo "Oranges are orange." ;;
+    *)
+        echo "Unknown fruit." ;;
+esac
+
