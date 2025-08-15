113016e60c030ff2d2d5d3e432d262b0d200001e
Ubuntu <ubuntu@ip-172-31-30-49.us-east-2.compute.internal>
2025-08-15 11:48:54 +0000

    Added my script

---
+++ script4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Number Comparsion
+ 
+read -p "Enter a number: " num
+
+if [ $num -gt 10 ];
+then
+    echo "Number is greater than 10"
+elif [ $num -eq 10 ];
+then
+    echo "Number is equal to 10"
+else
+    echo "Number is less than 10"
+fi
+
+
