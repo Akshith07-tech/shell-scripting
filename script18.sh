a5a0acd2938dc2c28d2b4f3ab5795a697ef0db9d
Ubuntu <ubuntu@ip-172-31-30-238.us-east-2.compute.internal>
2025-08-19 22:05:06 +0000

    Added my script

---
+++ script18.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# A simple countdown timer
+
+read -p "Enter number of seconds: " seconds
+
+while [ $seconds -gt 0 ];
+do
+    echo "Time left: $seconds seconds"
+    sleep 1
+    ((seconds--))
+done
+
+echo "Time's up!"
+
